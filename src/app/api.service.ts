262aa7de076f2c7c281afc57e25bf6891806aada
Mohamed Shokr <mohamedshokrr@gmail.com>
2018-07-12 11:47:48 +0200

    proxy, forms

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -13,7 +13,7 @@ export class ApiService {
   register(data){
     console.log("service called");
     console.log(data);
-    return this.http.post("http://172.104.209.122:3000/api/auth/user/signup", data).toPromise();
+    return this.http.post("/api/auth/user/signup", data).toPromise();
   }
 
   login(data){
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 
+import { User } from './../User';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,23 +12,66 @@ import { ApiService } from '../api.service';
 })
 export class RegisterComponent implements OnInit {
 
-  email: string = "";
-  name: string = "";
-  password: string = "";
+  email1: string = "";
+  name1: string = "";
+  password1: string = "";
 
-  constructor(private api: ApiService) { }
+  //Property for the user
+  private user: User;
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private api: ApiService) { }
 
   ngOnInit() {
+
+    this.signupForm = this.fb.group({
+      email: ['', [Validators.required,
+      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      name: ['', [Validators.required]],
+      password: this.fb.group({
+        pwd: ['', [Validators.required,
+        Validators.minLength(8)]],
+        confirmPwd: ['', [Validators.required,
+        Validators.minLength(8)]]
+      })
+    })
+
+  }
+
+  get email() { 
+    return this.signupForm.get('email'); 
+  }
+
+  get password() { 
+    return this.signupForm.get('password'); 
+  }
+
+  get name() {
+    return this.signupForm.get('name');
+  }
+
+  passwordConfirming(c: AbstractControl): { invalid: boolean } {
+    if (c.get('password').value !== c.get('confirm_password').value) {
+      return { invalid: true };
+    }
+  }
+
+
+  public onFormSubmit() {
+    if (this.signupForm.valid) {
+      this.user = this.signupForm.value;
+      console.log(this.user);
+    }
   }
 
   register(){
-    console.log(this.email);
-    console.log(this.name);
-    console.log(this.password);
+    console.log(this.email1);
+    console.log(this.name1);
+    console.log(this.password1);
     this.api.register({
-      "email" : this.email,
-      "name" : this.name,
-      "password" : this.password
+      "email" : this.email1,
+      "name" : this.name1,
+      "password" : this.password1
     }).then(data => {
       console.log(data);
     }).catch(err =>{
