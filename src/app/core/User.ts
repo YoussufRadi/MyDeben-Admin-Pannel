26549998cf3c4d6cb1e61aa356bd388518fc6e2a
Mohamed Shokr <mohamedshokrr@gmail.com>
2018-07-12 18:03:10 +0200

    login, register

--- src/app/core/User.ts
+++ src/app/core/User.ts
@@ -11,3 +11,4 @@ export class User {
     Object.assign(this, values);
   }
 }
+
--- src/app/core/services/api-manager.service.ts
+++ src/app/core/services/api-manager.service.ts
@@ -8,10 +8,10 @@ export class ApiManagerService {
   register(data) {
     console.log("service called");
     console.log(data);
-    return this.http.post("/api/auth/user/signup", data).toPromise();
+    return this.http.post("/api/auth/store/signup", data).toPromise();
   }
 
   login(data) {
-    return this.http.post("/api/login", data);
+    return this.http.post("/api/auth/store/signin", data).toPromise();
   }
 }
