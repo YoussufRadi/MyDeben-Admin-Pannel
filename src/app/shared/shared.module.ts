0bbd9d358adec3465fb4790e8e18ac4d82546212
Youssuf Radi <y.sameh1995@gmail.com>
2018-07-23 03:24:34 +0200

    Sort Working

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RequiredLabelDirective } from "./directives/required-label.directive";
-import { SearchPipe } from "./pipes/search.pipe";
+import { SearchPipe } from "./pipes/search.pipe/search.pipe";
+import { SortPipe } from "./pipes/order.pipe/sort.pipe";
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [RequiredLabelDirective, SearchPipe],
-  exports: [RequiredLabelDirective, SearchPipe]
+  declarations: [RequiredLabelDirective, SearchPipe, SortPipe],
+  exports: [RequiredLabelDirective, SearchPipe, SortPipe]
 })
 export class SharedModule {}
